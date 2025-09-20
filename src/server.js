173db65e54618774a19d99d689a3a713e9f7be3f
luan-devweb <luan44177@gmail.com>
2025-09-20 11:22:38 -0300

    chore: atualizando server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors"; // ✅ importar cors
+import cors from "cors"; //importar cors
 import adminRoutes from "./routes/adminRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoute.js";
@@ -9,10 +9,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// ✅ Middleware para parsear JSON
+//Middleware para parsear JSON
 app.use(express.json());
 
-// ✅ Configurar CORS (permite frontend acessar API)
+//Configurar CORS (permite frontend acessar API)
 app.use(
   cors({
     origin: "http://localhost:3000", // endereço do seu front
@@ -21,26 +21,26 @@ app.use(
   })
 );
 
-// ✅ Conectar ao MongoDB
+//Conectar ao MongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.PORTIFOLIO_URL);
-    console.log("✅ MongoDB connected");
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
 connectDB();
 
-// ✅ Rotas
+//Rotas
 app.use("/api/adm", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 
-// ✅ Subir servidor
+//Subir servidor
 app.listen(port, () => {
-  console.log(`🚀 Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 export default app;
